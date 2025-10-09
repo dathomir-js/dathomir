021a9056b4cd75bfa20ad08b1e875fd523c90f25
takuma-ru <kanekotakuma@team-lab.com>
2025-10-09 15:23:17 +0900

    ✨ main.tsx: Add todo management with async fetch and update functionality; enhance form styling in CSS

--- playgrounds/vanilla/src/main.tsx
+++ playgrounds/vanilla/src/main.tsx
@@ -1,23 +1,99 @@
 import "./style.css"
 import { computed, signal } from "@ailuros/core/reactivity"
 
-const firstName = signal("First")
-const lastName = signal("Last")
-const fullName = computed(() => firstName.value + (firstName.value ? " " : "") + lastName.value)
-const ff = computed(() => (arg: string) => { return arg === "Hello" ? <p><h1>Hello</h1></p> : <p>Goodbye</p> })
+const todo = signal<{
+  userId: number
+  id: number
+  title: string
+  body?: string
+} | undefined>(undefined)
+
+const postStatus = signal<"idle" | "loading" | "error" | "success">("idle")
+
+const postStatusText = computed(() => {
+  switch (postStatus.value) {
+    case "idle":
+      return "Idle"
+    case "loading":
+      return "Loading..."
+    case "error":
+      return <span style={{ color: "red" }}>Error!</span>
+    case "success":
+      return <span style={{ color: "green" }}>Success!</span>
+    default:
+      return ""
+  }
+})
+
+const updateTodo = async (arg: number) => {
+  postStatus.set("loading")
+
+  await fetch(`https://jsonplaceholder.typicode.com/posts/${arg}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      userId: 1,
+      id: arg,
+      title: todo.value?.title,
+      body: todo.value?.body,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then(res => res.json()).then(json => {
+    postStatus.set("success")
+    console.log(json)
+    todo.set(json)
+
+    setTimeout(() => {
+      postStatus.set("idle")
+    }, 2000)
+  }).catch(() => {
+    postStatus.set("error")
+  })
+
+  return;
+}
+
+const getTodo = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1").then(res => res.json())
+  todo.set(res)
+  return 0
+}
+await getTodo()
 
 const App = <>
-  <h1>{fullName.value}</h1>
-  <p>test</p>
+  <h1>@ailuros/core</h1>
   <div>
-    Hello, Ailuros!
-    <form action="">
-      <input name="firstName" type="text" value={firstName.value} onInput={(e) => e.target?.value && firstName.set(e.target.value)} />
-      <input name="lastName" type="text" value={lastName.value} onInput={(e) => e.target?.value && lastName.set(e.target.value)} />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        updateTodo(todo.value?.id || 1)
+      }}
+    >
+      <span>User ID : {todo.value?.userId}</span>
+      <span>ID : {todo.value?.id}</span>
+      <span>Title : {todo.value?.title}</span>
+      <label for="title">
+        <span style={{ marginRight: "0.5rem" }}>
+          Body :
+        </span>
+        <input
+          name="body"
+          type="text"
+          value={todo.value?.body || ""}
+          onInput={(e) => {
+            if (e.target?.value === undefined || todo.value === undefined) {
+              return;
+            }
+            todo.set({ ...todo.value, body: e.target.value})
+          }}
+        />
+      </label>
+      <button type="submit">Submit</button>
     </form>
-
-    <p>Your name is {fullName.value}</p>
-    {ff.value(firstName.value)}
+    <p>
+      {postStatusText.value}
+    </p>
   </div>
 </>
 
